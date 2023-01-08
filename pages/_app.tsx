bf1238fb817b3d1deff1856da9fdf3ca9dac7168
Hugo Moreira <hugo.n4@gmail.com>
2023-01-08 01:14:07 -0800

    troubleshoot next auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { Provider as NextAuthProvider } from "next-auth/providers"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
