24c2fb97b216f8587ed38fd46f3596593ee5e3ae
Hugo Moreira <hugo.n4@gmail.com>
2023-01-08 09:27:17 -0800

    next auth fix bug version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
-import { Provider as NextAuthProvider } from "next-auth/providers"
+import { Provider as NextAuthProvider } from "next-auth/client"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
