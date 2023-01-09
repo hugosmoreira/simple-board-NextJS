174cab19e518253f5519faf5fc05d9ae1839ba17
Hugo Moreira <hugo.n4@gmail.com>
2023-01-08 21:09:15 -0800

    login with github

--- components/SignInButton/index.tsx
+++ components/SignInButton/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
  
-import { signIn, signOut, useSession} from 'next-auth/react';
+import { signIn, signOut, useSession} from 'next-auth/client';
 
 
 
