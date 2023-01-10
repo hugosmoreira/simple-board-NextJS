1a1be5e95f9bbe3bb7a5c7d6871971a529e3a44b
Hugo Moreira <hugo.n4@gmail.com>
2023-01-10 11:53:53 -0800

    gravando somente tarefas no banco,  bug firebase

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,252 +1,167 @@
-import { useState, FormEvent } from 'react';
-import Head from 'next/head';
-import { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/client';
-import Link from 'next/link';
-
-import styles from './styles.module.scss'
+import Head from 'next/head'
 import { FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock, FiX } from 'react-icons/fi'
-import { SupportButton } from '../../components/SupportButton';
-import { format, formatDistance } from 'date-fns'
-import { ptBR } from 'date-fns/locale';
-import db from '../../services/firebaseConnection'
-
-import firebase from '../../services/firebaseConnection';
+import { SupportButton } from '../../components/SupportButton'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/client'
+import styles from './styles.module.scss'
+import { useState, FormEvent } from 'react'
+import firebase from '../../services/firebaseConnection'
 
 
 type TaskList = {
-  id: string;
-  created: string | Date;
-  createdFormated?: string;
-  tarefa: string;
-  userId: string;
-  nome: string;
-}
-
-interface BoardProps{
-  user:{
     id: string;
+    created: string | Date;
+    createdFormated?: string;
+    tarefa: string;
+    userId: string;
     nome: string;
-    vip: boolean;
-    lastDonate: string | Date;
   }
-  data: string;
-}
+  
+  interface BoardProps{
+    user:{
+      id: string;
+      nome: string;
+      vip: boolean;
+      lastDonate: string | Date;
+    }
+    data: string;
+  }
 
 
-export default function Board({ user, data }: BoardProps){
-  const [input, setInput] = useState('');
-  const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(data))
+export default function Board({ user}: BoardProps){
 
-  const [taskEdit, setTaskEdit] = useState<TaskList | null>(null)
+    const [input, setInput] = useState('');
+   
 
-  async function handleAddTask(e: FormEvent){
-    e.preventDefault();
+    async function handleAddTask(e: FormEvent) {
+        e.preventDefault();
+        if(input === ''){
+            alert('Preencha alguma tarefa!')
+            return;
+        }
 
-    if(input === ''){
-      alert('Preencha alguma tarefa!')
-      return;
+        await firebase.firestore().collection('tarefas')
+        .add({
+            created: new Date(),
+            tarefa: input,
+            // userId: user.id,
+            // nome: user.nome
+        })
+        .then((doc) => {
+            console.log('Cadastrado com sucesso')
+        })
+        .catch((err)=> {
+            console.log('Erro ao cadastrar')
+        })
+        
     }
 
-    if(taskEdit){
-
-      await firebase.firestore().collection('tarefas')
-      .doc(taskEdit.id)
-      .update({
-        tarefa: input
-      })
-      .then(()=>{
-        let data = taskList;
-        let taskIndex = taskList.findIndex(item => item.id === taskEdit.id);
-        data[taskIndex].tarefa = input
-
-        setTaskList(data);
-        setTaskEdit(null);
-        setInput('');
-
-      })
-
-      return;
-    }
-
-    await firebase.firestore().collection('tarefas')
-    .add({
-      created: new Date(),
-      tarefa: input,
-      userId: user.id,
-      nome: user.nome
-    })
-    .then((doc)=>{
-      console.log('CADASTRADO COM SUCESSO!');
-      let data = {
-        id: doc.id,
-        created: new Date(),
-        createdFormated: format(new Date(), 'dd MMMM yyyy'),
-        tarefa: input,
-        userId: user.id,
-        nome: user.nome
-      };
-
-      setTaskList([...taskList, data]);
-      setInput('');
-
-    })
-    .catch((err)=>{
-      console.log('ERRO AO CADASTRAR: ', err)
-    })
-
-  }
 
 
-  async function handleDelete(id: string){
-    
-    await firebase.firestore().collection('tarefas').doc(id)
-    .delete()
-    .then(()=>{
-      console.log('DELETADO COM SUCESSO!');
-      let taskDeleted = taskList.filter( item => {
-        return (item.id !== id)
-      });
+    return(
+       <>
 
-      setTaskList(taskDeleted);
+       <Head>
+            <title>My Tasks - Board</title>
+       </Head>
+        <main className={styles.container}>
+            <form onSubmit={handleAddTask}>
+                <input 
+                    type="text"
+                    placeholder='Digite sua tarefa...'
+                    value={input}
+                    onChange={ (e) => setInput(e.target.value) }
+                />
+                <button type='submit'>
+                    <FiPlus size={25} color='#17181f' />
+                </button>
+                
+            </form>
 
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+            <h1>Voce tem 2 tarefas!</h1>
 
-  }
+            <section>
+            <article className={styles.taskList}>
+                
+                <p>Aprender a criar projetos usando NextJS</p>
+              
+                <div className={styles.actions}>
+                <div>
+                    <div>
+                    <FiCalendar size={20} color="#FFB800"/>
+                    <time>17 de Julho de 2022</time>
+                    </div>
+                
+                    <button >
+                        <FiEdit2 size={20} color="#FFF" />
+                        <span>Editar</span>
+                    </button>
+                
+                </div>
 
+                <button>
+                    <FiTrash size={20} color="#FF3636" />
+                    <span>Excluir</span>
+                </button>
+                </div>
+            </article>
+            </section>
+        </main>
 
-  function handleEditTask(task: TaskList){
-    setTaskEdit(task);
-    setInput(task.tarefa);
-  }
-
-  function handleCancelEdit(){
-    setInput('');
-    setTaskEdit(null);
-  }
-
-  return(
-    <>
-    <Head>
-    	<title>Minhas tarefas - Board</title>
-    </Head>
-    <main className={styles.container}>
-
-      {taskEdit && (
-        <span className={styles.warnText}>
-          <button onClick={ handleCancelEdit }>
-           <FiX size={30} color="#FF3636" />
-          </button>
-          Você está editando uma tarefa!
-        </span>
-      )}
-
-
-      <form onSubmit={handleAddTask} >
-        <input 
-          type="text" 
-          placeholder="Digite sua tarefa..."
-          value={input}
-          onChange={ (e) => setInput(e.target.value) }
-        />
-        <button type="submit">
-          <FiPlus size={25} color="#17181f" />
-        </button>
-      </form>
-
-    <h1>Você tem {taskList.length} {taskList.length === 1 ? 'Tarefa' : 'Tarefas'}!</h1>
-
-    <section>
-      {taskList.map( task => (
-      <article key={task.id} className={styles.taskList}>
-        <Link href={`/board/${task.id}`}>
-         <p>{task.tarefa}</p>
-        </Link>
-        <div className={styles.actions}>
-          <div>
+        <div className={styles.vipContainer}>
+            <h3>Obrigado por apoiar esse projeto.</h3>
             <div>
-              <FiCalendar size={20} color="#FFB800"/>
-              <time>{task.createdFormated}</time>
+                <FiClock size={28} color="#FFF" />
+                <time>
+                    Última doação foi a 3 dias
+                </time>
             </div>
-          {user.vip && (
-          <button onClick={ () => handleEditTask(task) } >
-            <FiEdit2 size={20} color="#FFF" />
-            <span>Editar</span>
-          </button>
-          ) }
-          </div>
-
-          <button onClick={ () => handleDelete(task.id) }>
-            <FiTrash size={20} color="#FF3636" />
-            <span>Excluir</span>
-          </button>
         </div>
-      </article>
-      ))}
-    </section>
-
-    </main>
-
-    {user.vip && (
-    <div className={styles.vipContainer}>
-      <h3>Obrigado por apoiar esse projeto.</h3>
-      <div>
-        <FiClock size={28} color="#FFF" />
-        <time>
-          Última doação foi {formatDistance(new Date(user.lastDonate), new Date(), { locale: ptBR} )}
-        </time>
-      </div>
-    </div>
-    )}
-
-    <SupportButton/>
-
-    </>
-  )
+        <SupportButton />
+       </>
+    )
 }
 
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const session = await getSession({ req });
-
-  if(!session?.id){
-    //Se o user nao tiver logado vamos redirecionar.
-    return{
-      redirect:{
-        destination: '/',
-        permanent: false
+    const session = await getSession({ req });
+  
+    if(!session?.id){
+      //Se o user nao tiver logado vamos redirecionar.
+      return{
+        redirect:{
+          destination: '/',
+          permanent: false
+        }
       }
     }
-  }
-
-  const tasks = await firebase.firestore().collection('tarefas')
-  .where('userId', '==', session?.id)
-  .orderBy('created', 'asc').get();
-
-  const data = JSON.stringify(tasks.docs.map( u => {
-    return {
-      id: u.id,
-      createdFormated: format(u.data().created.toDate(), 'dd MMMM yyyy'),
-      ...u.data(),
+  
+    // const tasks = await firebase.firestore().collection('tarefas')
+    // .where('userId', '==', session?.id)
+    // .orderBy('created', 'asc').get();
+  
+    // const data = JSON.stringify(tasks.docs.map( u => {
+    //   return {
+    //     id: u.id,
+    //     createdFormated: format(u.data().created.toDate(), 'dd MMMM yyyy'),
+    //     ...u.data(),
+    //   }
+    // }))
+  
+    const user = {
+    //   nome: session?.user.name,
+      id: session?.id,
+      vip: session?.vip,
+      lastDonate: session?.lastDonate
     }
-  }))
-
-  const user = {
-    // nome: session?.user.name,
-    id: session?.id,
-    vip: session?.vip,
-    lastDonate: session?.lastDonate
-  }
-
-
-  return{
-    props:{
-      user,
-      data
+  
+  
+    return{
+      props:{
+        // user,
+        // data
+      }
     }
+  
   }
-
-}
+  
--- services/firebaseConnection.ts
+++ services/firebaseConnection.ts
@@ -1,19 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
- 
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5tSNFFPfcmSxqdkXJnACK191AYxmzvMw",
-  authDomain: "simple-board-2aeee.firebaseapp.com",
-  projectId: "simple-board-2aeee",
-  storageBucket: "simple-board-2aeee.appspot.com",
-  messagingSenderId: "568126686902",
-  appId: "1:568126686902:web:b818c2afd142a579628817",
-  measurementId: "G-26SQNF77CG"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
- 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
- 
-export default db
 
-
+// Initialize Firebase
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+export default firebase;
